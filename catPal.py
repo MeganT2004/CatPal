9111a85327c6d52f7a6bf80b87bebeaea58bcfcf
Megan Tierney <49157579+MeganT2004@users.noreply.github.com>
2026-01-31 19:38:39 +0000

    Final Proof of Concept
    Added three new cat sprites and three events, a left click to pet the cat, a right click to pop the cat down/up so it doesn't block the screen as much, and a mouse tracker to see if the user is idle for 30 seconds which will make the cat sleep until the mouse is moved (checked every 0.5s).
    
    Also made variables more consistent

--- catPal.py
+++ catPal.py
@@ -3,9 +3,12 @@
 from tkinter import *
 from PIL import Image, ImageTk, ImageFilter
 import time
-import os, glob
+import os
 import ctypes
 from ctypes import wintypes
+from pynput import mouse
+
+cat_busy = False
 
 #getting usable area (window excluding taskbar)
 def get_work_area():
@@ -50,11 +53,80 @@ window.attributes("-topmost", True)
 window.configure(bg=TRANSPARENT_COLOR)
 
 bgBase, wb, hb = imageLoad("cat2test.png")
+bgLClick, wc, hc = imageLoad("cat2love.png")
+bgRClick, wd, hd = imageLoad("cat2down.png")
+bgSleep, wd, hd = imageLoad("cat2sleep.png")
 
-imgBase = ImageTk.PhotoImage(bgBase)
+img_base = ImageTk.PhotoImage(bgBase)
+img_l_click = ImageTk.PhotoImage(bgLClick)
+img_r_click = ImageTk.PhotoImage(bgRClick)
+img_sleep = ImageTk.PhotoImage(bgSleep)
 
-label = Label(window, image=imgBase, bd=0, bg=TRANSPARENT_COLOR, highlightthickness=0)
+label = Label(window, image=img_base, bd=0, bg=TRANSPARENT_COLOR, highlightthickness=0)
 label.pack()
+
+#catLove Event (LClick, timer)
+def on_cat_click(event):
+    global cat_busy
+    cat_busy = True
+    label.config(image=img_l_click)
+    label.image = img_l_click
+
+    #after 2 seconds, restore
+    window.after(2000, restore_cat)
+
+def restore_cat():
+    global cat_busy
+    label.config(image=img_base)
+    label.image = img_base
+    cat_busy = False
+
+#CatDown event (RClick)
+def cat_down_click(event):
+    global cat_busy
+    cat_busy = True
+    label.config(image=img_r_click)
+    label.image = img_r_click
+
+    #after 2 seconds, restore
+    label.bind("<Button-3>", restore_cat_click)
+
+def restore_cat_click(event):
+    global cat_busy
+    label.config(image=img_base)
+    label.image = img_base
+    label.bind("<Button-3>", cat_down_click)
+    cat_busy = False
+
+label.bind("<Button-1>", on_cat_click)
+label.bind("<Button-3>", cat_down_click)
+
+#Cat Sleep timer (Mouse Idle)
+last_move_time = time.time()
+mouse_pos = None
+
+def on_move(x, y):
+    global last_move_time, mouse_pos
+    mouse_pos = (x, y)
+    last_move_time = time.time()
+
+listener = mouse.Listener(on_move=on_move)
+listener.start()
+
+IDLE_TIME = 30
+
+def go_to_sleep():
+    label.config(image=img_sleep)
+    label.image = img_sleep
+
+def check_idle():
+    current_time = time.time()
+    if not cat_busy:
+        if current_time - last_move_time > IDLE_TIME:
+            go_to_sleep()
+        else:
+            restore_cat()
+    window.after(500, check_idle)  # check every half second
 
 window.update_idletasks()
 try:
@@ -66,5 +138,5 @@ x = right - wb
 y = bottom - hb
 
 window.geometry(f"{wb}x{hb}+{x-300}+{y}")
-
+check_idle()
 window.mainloop()
