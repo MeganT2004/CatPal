5e01ad692db9cc5f9d8b3a61fba59bf9fe24aa92
Megan Tierney <49157579+MeganT2004@users.noreply.github.com>
2026-01-31 18:41:40 +0000

    Image cleanups and Taskbar anchoring
    Changed the canvas to a label within a small window anchored to the taskbar (gotten by using windows 'working area' dimensions) and added a proper cat png with image cleanup to remove any halos around the image, white outline is intentional as shown on the cat2test png file.

--- catPal.py
+++ catPal.py
@@ -4,16 +4,67 @@ from tkinter import *
 from PIL import Image, ImageTk, ImageFilter
 import time
 import os, glob
+import ctypes
+from ctypes import wintypes
 
-window = Tk()
-window.config(highlightbackground='#000000')
-canvas = Canvas(window, width=500, height=500, background='#000000', bd=0)
-canvas.pack()
-label = Label(canvas,borderwidth=0,bg='#000000')
+#getting usable area (window excluding taskbar)
+def get_work_area():
+    SPI_GETWORKAREA = 0x0030
+    rect = wintypes.RECT()
+    ctypes.windll.user32.SystemParametersInfoW(
+        SPI_GETWORKAREA,
+        0,
+        ctypes.byref(rect),
+        0
+    )
+    return rect.left, rect.top, rect.right, rect.bottom
+
+left, top, right, bottom = get_work_area()
+screen_w = right - left
+screen_h = bottom - top
+
+#Transparency setup
+TRANSPARENT_COLOR = "#ff00ff"
+CUTOUT_THRESHOLD = 60
+GROW = 1
+
+def imageLoad(PATH): #Image load/clean up edges/remove halo
+    pil = Image.open(PATH).convert("RGBA")
+    w, h = pil.size
+
+    r, g, b, a = pil.split()
+    mask = a.point(lambda px: 255 if px >= CUTOUT_THRESHOLD else 0)
+
+    if GROW > 1:
+        mask = mask.filter(ImageFilter.MaxFilter(GROW))
+
+    bg = Image.new("RGB", (w, h), TRANSPARENT_COLOR)
+    fg = pil.convert("RGB")
+    bg.paste(fg, (0, 0), mask)
+
+    return bg, w, h
+
+window = Tk() #configure window
 window.overrideredirect(True)
-window.wm_attributes('-transparentcolor','#000000')
-window.wm_attributes('-topmost', True)
-canvas.create_window(0, 0, anchor=NW, window=label)
-img = PhotoImage(file='testPNG.png')
-canvas.create_image(250, 250, image=img)
-mainloop()
+window.attributes("-topmost", True)
+window.configure(bg=TRANSPARENT_COLOR)
+
+bgBase, wb, hb = imageLoad("cat2test.png")
+
+imgBase = ImageTk.PhotoImage(bgBase)
+
+label = Label(window, image=imgBase, bd=0, bg=TRANSPARENT_COLOR, highlightthickness=0)
+label.pack()
+
+window.update_idletasks()
+try:
+    window.wm_attributes("-transparentcolor", TRANSPARENT_COLOR)
+except TclError:
+    pass
+
+x = right - wb
+y = bottom - hb
+
+window.geometry(f"{wb}x{hb}+{x-300}+{y}")
+
+window.mainloop()
